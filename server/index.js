46ed77a96fc9a97f97a63fc741fbedb298ab71aa
unknown <wilsonkaroki@gmail.com>
2021-12-08 11:36:03 +0300

    Database Connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,34 @@
 const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const routes = require('./routes/routes');
 const app = express();
 
-const port = process.env.PORT || 3001;
-app.listen(port,()=>{
-    console.log(`Server starting at port ${port}`);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+    name: 'session',
+    secret: 'my_secret is mine and mine alone',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 3600 * 1000, // 1hr
+    }
+}));
+
+//running static files 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    return res.send('Internal Server Error');
 });
+
+const port = process.env.PORT || 5000;
+app.listen(port, ()=>console.log(`Server starting at port ${port}`));
