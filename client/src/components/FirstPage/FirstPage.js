1f0cbf2cf24826de429267aae53685a62e95699c
unknown <wilsonkaroki@gmail.com>
2021-12-16 18:06:24 +0300

    Responsiveness,Animations and backend modifications

--- client/src/components/FirstPage/FirstPage.js
+++ client/src/components/FirstPage/FirstPage.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import "./First.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
+import { Wave } from 'react-animated-text';
+import { Random } from 'react-animated-text';
 
 const FirstPage = () => {
   useEffect(() => {
@@ -13,27 +15,18 @@ const FirstPage = () => {
         <h1>Rent</h1>
       </div>
       <div className="first" data-aos="fade-down" data-aos-delay="50">
-        <h1>WELCOME TO NEW VILE</h1>
+        <h1><Random text="WELCOME TO NEW VILE"/> </h1>
       </div>
       <div className="second" data-aos="fade-up" data-aos-delay="50">
         <h2>
-          The best Car <br />
-          Rental Services
+        <Random text=" The best Car Rental Services"/>
         </h2>
       </div>
-      <div class="custom-shape-divider-bottom-1638984573">
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-        >
-          <path
-            d="M602.45,3.86h0S572.9,116.24,281.94,120H923C632,116.24,602.45,3.86,602.45,3.86Z"
-            class="shape-fill"
-          ></path>
-        </svg>
-      </div>
+      <div class="custom-shape-divider-bottom-1639666289">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+    </svg>
+</div>
     </div>
   );
 };
